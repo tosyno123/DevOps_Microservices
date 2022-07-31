6853dd131cab661a71b8e50a3f10086edd527a7b
tosyno123 <oluwatomisin.owolabi@yahoo.com>
2022-07-31 05:58:28 +0100

    updating docker and kubernetes files

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=tomisiin/mlapi
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker login
+kubectl run mlapi --image=$dockerpath:v1.0.0 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mlapi 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=tomisiin/mlapi
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag mlapi $dockerpath:v1.0.0
 
 # Step 3:
 # Push image to a docker repository
+docker login
+docker push $dockerpath:v1.0.0
